71866d7c948b0f63756865ac8487d1166407f295
Serge Zhigunov <serge.zhigunov@outlook.com>
2016-04-21 16:54:44 +0500

    Add export function stubs

--- src/GostCsp/gostcsp.cpp
+++ src/GostCsp/gostcsp.cpp
@@ -1,3 +1,122 @@
 #include "stdafx.h"
 #include "gostcsp.h"
 
+BOOL WINAPI CPAcquireContext(HCRYPTPROV * phProv, CHAR * pszContainer, DWORD dwFlags, PVTableProvStruc pVTable)
+{
+    return E_NOTIMPL;
+}
+
+BOOL WINAPI CPCreateHash(HCRYPTPROV hProv, ALG_ID Algid, HCRYPTKEY hKey, DWORD dwFlags, HCRYPTHASH * phHash)
+{
+    return E_NOTIMPL;
+}
+
+BOOL WINAPI CPDecrypt(HCRYPTPROV hProv, HCRYPTKEY hKey, HCRYPTHASH hHash, BOOL Final, DWORD dwFlags, BYTE * pbData, DWORD * pdwDataLen)
+{
+    return E_NOTIMPL;
+}
+
+BOOL WINAPI CPDestroyHash(HCRYPTPROV hProv, HCRYPTHASH hHash)
+{
+    return E_NOTIMPL;
+}
+
+BOOL WINAPI CPDestroyKey(HCRYPTPROV hProv, HCRYPTKEY hKey)
+{
+    return E_NOTIMPL;
+}
+
+BOOL WINAPI CPDuplicateHash(HCRYPTPROV hProv, HCRYPTHASH hHash, DWORD * pdwReserved, DWORD dwFlags, HCRYPTHASH * phHash)
+{
+    return E_NOTIMPL;
+}
+
+BOOL WINAPI CPDuplicateKey(HCRYPTPROV hUID, HCRYPTKEY hKey, DWORD * pdwReserved, DWORD dwFlags, HCRYPTKEY * phKey)
+{
+    return E_NOTIMPL;
+}
+
+BOOL WINAPI CPEncrypt(HCRYPTPROV hProv, HCRYPTKEY hKey, HCRYPTHASH hHash, BOOL Final, DWORD dwFlags, BYTE * pbData, DWORD * pdwDataLen, DWORD dwBufLen)
+{
+    return E_NOTIMPL;
+}
+
+BOOL WINAPI CPExportKey(HCRYPTPROV hProv, HCRYPTKEY hKey, HCRYPTKEY hPubKey, DWORD dwBlobType, DWORD dwFlags, BYTE * pbData, DWORD * pdwDataLen)
+{
+    return E_NOTIMPL;
+}
+
+BOOL WINAPI CPGenKey(HCRYPTPROV hProv, ALG_ID Algid, DWORD dwFlags, HCRYPTKEY * phKey)
+{
+    return E_NOTIMPL;
+}
+
+BOOL WINAPI CPGenRandom(HCRYPTPROV hProv, DWORD dwLen, BYTE * pbBuffer)
+{
+    return E_NOTIMPL;
+}
+
+BOOL WINAPI CPGetHashParam(HCRYPTPROV hProv, HCRYPTHASH hHash, DWORD dwParam, BYTE * pbData, DWORD * pdwDataLen, DWORD dwFlags)
+{
+    return E_NOTIMPL;
+}
+
+BOOL WINAPI CPGetKeyParam(HCRYPTPROV hProv, HCRYPTKEY hKey, DWORD dwParam, LPBYTE pbData, LPDWORD pcbDataLen, DWORD dwFlags)
+{
+    return E_NOTIMPL;
+}
+
+BOOL WINAPI CPGetProvParam(HCRYPTPROV hProv, DWORD dwParam, BYTE * pbData, DWORD * pdwDataLen, DWORD dwFlags)
+{
+    return E_NOTIMPL;
+}
+
+BOOL WINAPI CPGetUserKey(HCRYPTPROV hProv, DWORD dwKeySpec, HCRYPTKEY * phUserKey)
+{
+    return E_NOTIMPL;
+}
+
+BOOL WINAPI CPHashData(HCRYPTPROV hProv, HCRYPTHASH hHash, const BYTE * pbData, DWORD dwDataLen, DWORD dwFlags)
+{
+    return E_NOTIMPL;
+}
+
+BOOL WINAPI CPHashSessionKey(HCRYPTPROV hProv, HCRYPTHASH hHash, HCRYPTKEY hKey, DWORD dwFlags)
+{
+    return E_NOTIMPL;
+}
+
+BOOL WINAPI CPImportKey(HCRYPTPROV hProv, const BYTE * pbData, DWORD dwDataLen, HCRYPTKEY hPubKey, DWORD dwFlags, HCRYPTKEY * phKey)
+{
+    return 0;
+}
+
+BOOL WINAPI CPReleaseContext(HCRYPTPROV hProv, DWORD dwFlags)
+{
+    return E_NOTIMPL;
+}
+
+BOOL WINAPI CPSetHashParam(HCRYPTPROV hProv, HCRYPTHASH hHash, DWORD dwParam, BYTE * pbData, DWORD dwFlags)
+{
+    return 0;
+}
+
+BOOL WINAPI CPSetKeyParam(HCRYPTPROV hProv, HCRYPTKEY hKey, DWORD dwParam, BYTE * pbData, DWORD dwFlags)
+{
+    return 0;
+}
+
+BOOL WINAPI CPSetProvParam(HCRYPTPROV hProv, DWORD dwParam, BYTE * pbData, DWORD dwFlags)
+{
+    return 0;
+}
+
+BOOL WINAPI CPSignHash(HCRYPTPROV hProv, HCRYPTHASH hHash, DWORD dwKeySpec, LPCWSTR sDescription, DWORD dwFlags, BYTE * pbSignature, DWORD * pdwSigLen)
+{
+    return 0;
+}
+
+BOOL WINAPI CPVerifySignature(HCRYPTPROV hProv, HCRYPTHASH hHash, const BYTE * pbSignature, DWORD dwSigLen, HCRYPTKEY hPubKey, LPCWSTR sDescription, DWORD dwFlags)
+{
+    return 0;
+}
--- src/GostCsp/gostcsp.h
+++ src/GostCsp/gostcsp.h
@@ -1,2 +1,236 @@
-#define EXPORT __declspec(dllexport)
+#pragma once
 
+#include <wincrypt.h>
+
+#define EXPORT EXTERN_C __declspec(dllexport) 
+
+#pragma region Callback definitions
+
+typedef BOOL(WINAPI *CRYPT_VERIFY_IMAGE_A)(LPCSTR  szImage, CONST BYTE *pbSigData);
+typedef BOOL(WINAPI *CRYPT_VERIFY_IMAGE_W)(LPCWSTR szImage, CONST BYTE *pbSigData);
+typedef void(*CRYPT_RETURN_HWND)(HWND *phWnd);
+
+#pragma endregion
+
+#pragma region Struct definition
+
+typedef struct _VTableProvStruc {
+    DWORD                Version;
+    CRYPT_VERIFY_IMAGE_A FuncVerifyImage;
+    CRYPT_RETURN_HWND    FuncReturnhWnd;
+    DWORD                dwProvType;
+    BYTE                *pbContextInfo;
+    DWORD                cbContextInfo;
+    LPSTR                pszProvName;
+} VTableProvStruc, *PVTableProvStruc;
+
+typedef struct _VTableProvStrucW {
+    DWORD                Version;
+    CRYPT_VERIFY_IMAGE_W FuncVerifyImage;
+    CRYPT_RETURN_HWND    FuncReturnhWnd;
+    DWORD                dwProvType;
+    BYTE                *pbContextInfo;
+    DWORD                cbContextInfo;
+    LPWSTR               pszProvName;
+} VTableProvStrucW, *PVTableProvStrucW;
+
+typedef struct {
+    DWORD dwVersion;
+    DWORD dwCrcOffset;
+    BYTE rgbSignature[88];  // 1024-bit key, plus 2 DWORDs of padding.
+} InFileSignatureResource;
+
+#pragma endregion
+
+#pragma region Export function definition
+
+EXPORT BOOL WINAPI CPAcquireContext(
+    _Out_  HCRYPTPROV *phProv,
+    _In_   CHAR *pszContainer,
+    _In_   DWORD dwFlags,
+    _In_   PVTableProvStruc pVTable
+);
+
+EXPORT BOOL WINAPI CPCreateHash(
+    _In_   HCRYPTPROV hProv,
+    _In_   ALG_ID Algid,
+    _In_   HCRYPTKEY hKey,
+    _In_   DWORD dwFlags,
+    _Out_  HCRYPTHASH *phHash
+);
+
+EXPORT BOOL WINAPI CPDecrypt(
+    _In_     HCRYPTPROV hProv,
+    _In_     HCRYPTKEY hKey,
+    _In_     HCRYPTHASH hHash,
+    _In_     BOOL Final,
+    _In_     DWORD dwFlags,
+    _Inout_  BYTE *pbData,
+    _Inout_  DWORD *pdwDataLen
+);
+
+EXPORT BOOL WINAPI CPDestroyHash(
+    _In_  HCRYPTPROV hProv,
+    _In_  HCRYPTHASH hHash
+);
+
+EXPORT BOOL WINAPI CPDestroyKey(
+    _In_  HCRYPTPROV hProv,
+    _In_  HCRYPTKEY hKey
+);
+
+EXPORT BOOL WINAPI CPDuplicateHash(
+    _In_        HCRYPTPROV hProv,
+    _In_        HCRYPTHASH hHash,
+    _Reserved_  DWORD *pdwReserved,
+    _In_        DWORD dwFlags,
+    _Out_       HCRYPTHASH *phHash
+);
+
+EXPORT BOOL WINAPI CPDuplicateKey(
+    _In_   HCRYPTPROV hUID,
+    _In_   HCRYPTKEY hKey,
+    _In_   DWORD *pdwReserved,
+    _In_   DWORD dwFlags,
+    _Out_  HCRYPTKEY *phKey
+);
+
+EXPORT BOOL WINAPI CPEncrypt(
+    _In_     HCRYPTPROV hProv,
+    _In_     HCRYPTKEY hKey,
+    _In_     HCRYPTHASH hHash,
+    _In_     BOOL Final,
+    _In_     DWORD dwFlags,
+    _Inout_  BYTE *pbData,
+    _Inout_  DWORD *pdwDataLen,
+    _In_     DWORD dwBufLen
+);
+
+EXPORT BOOL WINAPI CPExportKey(
+    _In_     HCRYPTPROV hProv,
+    _In_     HCRYPTKEY hKey,
+    _In_     HCRYPTKEY hPubKey,
+    _In_     DWORD dwBlobType,
+    _In_     DWORD dwFlags,
+    _Out_    BYTE *pbData,
+    _Inout_  DWORD *pdwDataLen
+);
+
+EXPORT BOOL WINAPI CPGenKey(
+    _In_   HCRYPTPROV hProv,
+    _In_   ALG_ID Algid,
+    _In_   DWORD dwFlags,
+    _Out_  HCRYPTKEY *phKey
+);
+
+EXPORT BOOL WINAPI CPGenRandom(
+    _In_     HCRYPTPROV hProv,
+    _In_     DWORD dwLen,
+    _Inout_  BYTE *pbBuffer
+);
+
+EXPORT BOOL WINAPI CPGetHashParam(
+    _In_     HCRYPTPROV hProv,
+    _In_     HCRYPTHASH hHash,
+    _In_     DWORD dwParam,
+    _Out_    BYTE *pbData,
+    _Inout_  DWORD *pdwDataLen,
+    _In_     DWORD dwFlags
+);
+
+EXPORT BOOL WINAPI CPGetKeyParam(
+    _In_     HCRYPTPROV hProv,
+    _In_     HCRYPTKEY hKey,
+    _In_     DWORD dwParam,
+    _Out_    LPBYTE pbData,
+    _Inout_  LPDWORD pcbDataLen,
+    _In_     DWORD dwFlags
+);
+
+EXPORT BOOL WINAPI CPGetProvParam(
+    _In_     HCRYPTPROV hProv,
+    _In_     DWORD dwParam,
+    _Out_    BYTE *pbData,
+    _Inout_  DWORD *pdwDataLen,
+    _In_     DWORD dwFlags
+);
+
+EXPORT BOOL WINAPI CPGetUserKey(
+    _In_   HCRYPTPROV hProv,
+    _In_   DWORD dwKeySpec,
+    _Out_  HCRYPTKEY *phUserKey
+);
+
+EXPORT BOOL WINAPI CPHashData(
+    _In_  HCRYPTPROV hProv,
+    _In_  HCRYPTHASH hHash,
+    _In_  const BYTE *pbData,
+    _In_  DWORD dwDataLen,
+    _In_  DWORD dwFlags
+);
+
+EXPORT BOOL WINAPI CPHashSessionKey(
+    _In_  HCRYPTPROV hProv,
+    _In_  HCRYPTHASH hHash,
+    _In_  HCRYPTKEY hKey,
+    _In_  DWORD dwFlags
+);
+
+EXPORT BOOL WINAPI CPImportKey(
+    _In_   HCRYPTPROV hProv,
+    _In_   const BYTE *pbData,
+    _In_   DWORD dwDataLen,
+    _In_   HCRYPTKEY hPubKey,
+    _In_   DWORD dwFlags,
+    _Out_  HCRYPTKEY *phKey
+);
+
+EXPORT BOOL WINAPI CPReleaseContext(
+    _In_  HCRYPTPROV hProv,
+    _In_  DWORD dwFlags
+);
+
+EXPORT BOOL WINAPI CPSetHashParam(
+    _In_  HCRYPTPROV hProv,
+    _In_  HCRYPTHASH hHash,
+    _In_  DWORD dwParam,
+    _In_  BYTE *pbData,
+    _In_  DWORD dwFlags
+);
+
+EXPORT BOOL WINAPI CPSetKeyParam(
+    _In_  HCRYPTPROV hProv,
+    _In_  HCRYPTKEY hKey,
+    _In_  DWORD dwParam,
+    _In_  BYTE *pbData,
+    _In_  DWORD dwFlags
+);
+
+EXPORT BOOL WINAPI CPSetProvParam(
+    _In_  HCRYPTPROV hProv,
+    _In_  DWORD dwParam,
+    _In_  BYTE *pbData,
+    _In_  DWORD dwFlags
+);
+
+EXPORT BOOL WINAPI CPSignHash(
+    _In_     HCRYPTPROV hProv,
+    _In_     HCRYPTHASH hHash,
+    _In_     DWORD dwKeySpec,
+    _In_     LPCWSTR sDescription,
+    _In_     DWORD dwFlags,
+    _Out_    BYTE *pbSignature,
+    _Inout_  DWORD *pdwSigLen
+);
+
+EXPORT BOOL WINAPI CPVerifySignature(
+    _In_  HCRYPTPROV hProv,
+    _In_  HCRYPTHASH hHash,
+    _In_  const BYTE *pbSignature,
+    _In_  DWORD dwSigLen,
+    _In_  HCRYPTKEY hPubKey,
+    _In_  LPCWSTR sDescription,
+    _In_  DWORD dwFlags
+);
+
+#pragma endregion
